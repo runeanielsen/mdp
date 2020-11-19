9b17ed9bd78621e9fd0874588352807071309c79
Rune Nielsen <runenielsen@runbox.com>
2020-11-19 22:30:36 +0100

    adds automatic preview of the generated html file

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
+	"os/exec"
 
 	"github.com/microcosm-cc/bluemonday"
 	"github.com/russross/blackfriday/v2"
@@ -29,6 +30,7 @@ const (
 
 func main() {
 	filename := flag.String("file", "", "Markdown file to preview")
+	skipPreview := flag.Bool("s", false, "Skip auto-preview")
 	flag.Parse()
 
 	if *filename == "" {
@@ -36,12 +38,12 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err := run(*filename, os.Stdout); err != nil {
+	if err := run(*filename, os.Stdout, *skipPreview); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 	}
 }
 
-func run(filename string, out io.Writer) error {
+func run(filename string, out io.Writer, skipPreview bool) error {
 	input, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return err
@@ -61,7 +63,15 @@ func run(filename string, out io.Writer) error {
 
 	fmt.Fprintln(out, outName)
 
-	return saveHTML(outName, htmlData)
+	if err := saveHTML(outName, htmlData); err != nil {
+		return err
+	}
+
+	if skipPreview {
+		return nil
+	}
+
+	return preview(outName)
 }
 
 func parseContent(input []byte) []byte {
@@ -79,4 +89,17 @@ func parseContent(input []byte) []byte {
 
 func saveHTML(outFname string, data []byte) error {
 	return ioutil.WriteFile(outFname, data, 0644)
+}
+
+func preview(fname string) error {
+	browserPath, err := exec.LookPath("firefox")
+	if err != nil {
+		return err
+	}
+
+	if err := exec.Command(browserPath, fname).Start(); err != nil {
+		return err
+	}
+
+	return nil
 }
